6710bbbf0ff1e3da2fdb0969dbb3a36b5e038d14
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2024-12-29 20:28:42 +0700

    Menambahkan insert view model

---
+++ app/src/main/java/com/example/praktikum10/viewmodel/InsertViewModel.kt
@@ -0,0 +1,56 @@
+package com.example.praktikum10.viewmodel
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.praktikum10.model.Mahasiswa
+import com.example.praktikum10.repository.MahasiswaRepository
+
+class InsertViewModel(private val mhs: MahasiswaRepository): ViewModel(){
+    var uiState by mutableStateOf(InsertUiState)
+}
+
+suspend fun insertMhs(){
+    viewModelScope.launch{
+        try {
+            mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+        }
+        catch (e: Exception){
+            e.printStackTrace()
+        }
+    }
+}
+
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+)
+
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
+fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
+fun Mahasiswa.toUiStateMhs():insertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent()
+)
+
+fun Mahasiswa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
