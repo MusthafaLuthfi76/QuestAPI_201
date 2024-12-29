8f411fc7728376203e19ecf47835f00e2bf738f1
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2024-12-30 00:18:11 +0700

    Menambahkan MahasiswaApp.kt

--- app/src/main/java/com/example/praktikum10/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/praktikum10/ui/viewmodel/PenyediaViewModel.kt
@@ -1,13 +1,10 @@
-package com.example.praktikum10.ui
+package com.example.praktikum10.ui.viewmodel
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.praktikum10.MahasiswaApplications
-import com.example.praktikum10.ui.viewmodel.HomeViewModel
-import com.example.praktikum10.ui.viewmodel.InsertViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
