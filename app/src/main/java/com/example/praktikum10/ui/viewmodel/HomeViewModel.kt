54f2a9abab5033ab802046bed2c43b4f7bc3a683
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2024-12-30 09:51:31 +0700

    Memperbaiki error di HomeViewModel.kt

--- app/src/main/java/com/example/praktikum10/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/praktikum10/ui/viewmodel/HomeViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.praktikum10.ui.viewmodel
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import coil.network.HttpException
@@ -11,13 +13,12 @@ import okio.IOException
 
 sealed class HomeUiState{
     data class Success(val mahasiswa: List<Mahasiswa>): HomeUiState()
-
     object Error: HomeUiState()
     object Loading: HomeUiState()
 }
 
 class HomeViewModel(private val mhs: MahasiswaRepository): ViewModel(){
-    var mhsUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+    var mhsUIState: HomeUiState by mutableStateOf(HomeUiState.Loading)
         private set
 
     init {
