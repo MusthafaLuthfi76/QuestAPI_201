7872f666bbd110295f6bbc449c54410a3ec6c072
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2024-12-30 09:45:18 +0700

    Memperbaiki error di InsertView.kt

--- app/src/main/java/com/example/praktikum10/ui/view/InsertView.kt
+++ app/src/main/java/com/example/praktikum10/ui/view/InsertView.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
@@ -13,12 +14,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.praktikum10.ui.customwidget.CostumeTopAppBar
@@ -59,9 +60,9 @@ fun EntryMhsScreen(
         innerPadding ->
         EntryBody(
             insertUiState = viewModel.uiState,
-            onSiswaValueChange = viewModel::updateInsertMhsState,
+            onSiswaValueChange = viewModel :: updateInsertMhsState,
             onSaveClick = {
-                coroutineScope.launch{
+                coroutineScope.launch {
                     viewModel.insertMhs()
                     navigateBack()
                 }
@@ -77,7 +78,7 @@ fun EntryMhsScreen(
 @Composable
 fun EntryBody(
     insertUiState: InsertUiState,
-    onSiswaValueChange: (InsertUiEvent) -> Unit
+    onSiswaValueChange: (InsertUiEvent) -> Unit,
     onSaveClick: () -> Unit,
     modifier: Modifier = Modifier
 ){
@@ -86,7 +87,7 @@ fun EntryBody(
         modifier = modifier.padding(12.dp)
     ){
         FormInput(
-            insertUiState = insertUiState.insertUiEvent,
+            insertUiEvent = insertUiState.insertUiEvent,
             onValueChange = onSiswaValueChange,
             modifier = Modifier.fillMaxWidth()
         )
