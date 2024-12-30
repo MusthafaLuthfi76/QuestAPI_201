dbb5d722fbefc15e9f8998f847f8bd4a34d3bf88
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2024-12-30 09:47:47 +0700

    Memperbaiki error di InsertViewViewModel.kt

--- app/src/main/java/com/example/praktikum10/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/praktikum10/ui/viewmodel/InsertViewModel.kt
@@ -1,21 +1,31 @@
 package com.example.praktikum10.ui.viewmodel
 
+
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.praktikum10.model.Mahasiswa
 import com.example.praktikum10.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
 
-class InsertViewModel(private val mhs: MahasiswaRepository): ViewModel(){
-    var uiState by mutableStateOf(InsertUiState)
-}
+class InsertViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
 
-suspend fun insertMhs(){
-    viewModelScope.launch{
-        try {
-            mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
-        }
-        catch (e: Exception){
-            e.printStackTrace()
+    var uiState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent) {
+        uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertMhs() {
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
 }
@@ -42,7 +52,7 @@ fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
     angkatan = angkatan
 )
 
-fun Mahasiswa.toUiStateMhs():insertUiState = InsertUiState(
+fun Mahasiswa.toUiStateMhs(): InsertUiState = InsertUiState(
     insertUiEvent = toInsertUiEvent()
 )
 
